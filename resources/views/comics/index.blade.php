59e9edd9d709cd14d7537301e309fdeb42ca60b1
Runatorium <stefano.alesci1@gmail.com>
2023-05-16 18:01:20 +0200

    Card display

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,19 @@
 @section('page-title', 'lista fumetti')
 
 @section('content')
-
+    <div class="row">
+        @foreach ($comics as $comic)
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h2 class="card-text">{{ $comic->title }}</h2>
+                        <p>{{ $comic->descripion }}</p>
+                        <p>${{ $comic->price }}</p>
+                        <p>{{ $comic->series }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
