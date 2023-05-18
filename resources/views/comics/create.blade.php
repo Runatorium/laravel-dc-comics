6b8c4e31e40b552a0bffb2b1509fa99d927864d5
Runatorium <stefano.alesci1@gmail.com>
2023-05-18 16:19:26 +0200

    Save data with 'Old()' method

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,7 +11,8 @@
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Url dell'immagine</label>
-            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb"
+                value="{{ old('thumb') }}">
             @error('thumb')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -19,7 +20,8 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                value="{{ old('title') }}">
             @error('title')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -27,7 +29,8 @@
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                value="{{ old('price') }}">
             @error('price')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -35,7 +38,8 @@
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series"
+                value="{{ old('series') }}">
             @error('series')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -43,8 +47,8 @@
 
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control @error('description') is-invalid @enderror" id="description"
-                name="description">
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                name="description">{{ old('description') }}</textarea>
             @error('description')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -52,14 +56,15 @@
         <div class="mb-3">
             <label for="sale_date" class="form-label">date</label>
             <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date"
-                name="sale_date">
+                name="sale_date" value="{{ old('sale_date') }}">
             @error('sale_date')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Tipo</label>
-            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type"
+                value="{{ old('type') }}">
             @error('type')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
