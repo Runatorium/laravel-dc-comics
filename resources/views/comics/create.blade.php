d76e08f887eddc50dcc9ac863c9085be46c5efb1
Runatorium <stefano.alesci1@gmail.com>
2023-05-18 16:14:17 +0200

    Imput Error message added for "Form fields"

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,35 +11,58 @@
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Url dell'immagine</label>
-            <input type="text" class="form-control" id="thumb" name="thumb">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb">
+            @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price">
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" id="series" name="series">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series">
+            @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control" id="description" name="description">
+            <input type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                name="description">
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">date</label>
-            <input type="date" class="form-control" id="sale_date" name="sale_date">
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date"
+                name="sale_date">
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="type" name="type">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type">
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Salva</button>
